58ad2f928296db19f81594940add0271e560ceda
gaxmaleano <maxagaleano@gmail.com>
2025-01-23 17:23:27 -0300

    v0.0.4 - Bassed on RH tampleate

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
-def get_data():
-    return jsonify({"message": "MEL RESPONSE"})
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')
